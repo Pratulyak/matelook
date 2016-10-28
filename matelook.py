96b82f2789d1808d91854598d2ab7fc2cefd58aa
System Administrator <root@Admins-MacBook-Pro.local>
2016-10-28 13:26:46 +1100

    halfway there

--- matelook.py
+++ matelook.py
@@ -35,24 +35,80 @@ def login():
 			longitude = temp[9]
 			latitude = temp[10]
 			birthday = temp[11]
-			image =  temp[12]
+			image =  "dataset/" + zid + "/profile.jpg"#temp[12]
 			password=temp[3]
-
+		curs.execute("select zidB,profile from mates where zidA = (?)",(zid,))
+		mates = []
+		mateImages= []
+		for row in curs.fetchall():
+			mates.append(row[0])
+			mateImages.append(row[1])
 
 	if password == passw:
 		session['user'] = zid
 		g.user = session['user']
 		return render_template("profile.html", zid=zid, image=image,name=fullname,email=email,program=program,suburb=homesuburb
-		,longitude=longitude,latitude=latitude,birthday=birthday)
-
+		,longitude=longitude,latitude=latitude,birthday=birthday,mates=mates,mateImages=mateImages)
 	else:
 		return render_template("login.html",error=error)
+
+
+@app.route('/viewProfile',methods=['POST','GET'])
+def viewProfile():
+
+	temp = request.form['profileID']
+	root = request.form['rootID']
+	submit = request.form['profileID']
+	if temp == root:
+		submit = request.form['rootID']
+
+	return render_template("guess.html",submit=submit,rootid=root)
+"""	if request.form['submit'] == "My Profile":
+		curs.execute("select * from User where zid = (?)",(root,))
+	elif request.form['submit'] == "View Profile":
+		curs.execute("select * from User where zid = (?)",(temp,))
+	for temp in curs.fetchall():
+			#temp = curs.fetchall()
+			#temp =temp.split(",")
+		zid = temp[0]
+		fullname = temp[1]
+		email = temp[2]
+		program = temp[4]
+		homesuburb= temp[6]
+		longitude = temp[9]
+		latitude = temp[10]
+		birthday = temp[11]
+		image =  "dataset/" + zid + "/profile.jpg"#temp[12]
+		password=temp[3]
+	curs.execute("select zidB,profile from mates where zidA = (?)",(zid,))
+	mates = []
+	mateImages= []
+	for row in curs.fetchall():
+		mates.append(row[0])
+		mateImages.append(row[1])
+
 	
+	session['user'] = zid
+	g.user = session['user']
+	return render_template("profile.html",rootid=root, zid=zid, image=image,name=fullname,email=email,program=program,suburb=homesuburb
+		,longitude=longitude,latitude=latitude,birthday=birthday,mates=mates,mateImages=mateImages)
+	"""
+	
+
 @app.route('/logout',methods=['POST','GET'])
 def logout():
 	#zid = session['user']
 	session.pop('user', None)
 	return render_template("login.html")
+
+@app.route('/search',methods=['POST','GET'])
+def search():
+	searchString = request.form['matesearch']
+	searchString += '%'
+	root = request.form['profileID']
+	curs.execute("select * from User where full_name LIKE (?)",(searchString,))
+	searchList = curs.fetchall()
+	return render_template('searchResults.html',results=searchList,zid=root)
 # your css, js, images, and anything that you wouldn't want CGI to execute, but the user should see go in the "static" directory during testing
 # During production, it's advisable to set this up to be served by apache instead
 @app.route('/static/<path:path>')
